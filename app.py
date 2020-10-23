453a9cc7ca174d7d99e4a466ca253de1bd8d42c0
rickyalvin <rickyalvin11@gmail.com>
2020-10-23 15:40:42 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 import requests
 # import json
@@ -6,42 +6,34 @@ import requests
 @app.route("/",  methods = ['GET'])
 def home():
     return "Hello, World!"
-
-
-# def auth_check():
-#     url = 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F'
-#     headers = {'Metadata' : true}
-#     r = requests.get(url, headers=headers)
-#     response = json.loads(r.content)
-#     return response['access_token']
     
+def json_parse(request):
+	req_data = request.get_json()
+	client_id = req_data['client_id']
+	client_secret = req_data['client_secret']
     
+    return client_id, client_secret
 
 @app.route('/auth', methods = ['POST'])
 def get_sas_token():
     import adal
 
-    tenant_id = '24041256-c834-468a-b7e7-c4dc34322cfe'
-
-    resource = 'https://storagetestcz.blob.core.windows.net'
-
-    application_id = 'acb4395e-5f21-4001-9a9d-bc83664fedbc'
-    application_secret = 'CNjT8dMmVReuDHK.SVea11XIp-~aPr_60F'
     authentication_endpoint = 'https://login.microsoftonline.com/'
     resource  = 'https://management.core.windows.net/'
+    storage_account = 'storagetestcz'
+    tenant_id = '24041256-c834-468a-b7e7-c4dc34322cfe'
+    
+    resource = 'https://'+storage_account+'.blob.core.windows.net'
+    
+    application_id ,application_secret = json_parse(request)
 
-# get an Azure access token using the adal library
+    # get an Azure access token using the adal library
     context = adal.AuthenticationContext(authentication_endpoint + tenant_id)
     token_response = context.acquire_token_with_client_credentials(resource, application_id, application_secret)
 
     access_token = token_response.get('accessToken')
-    print(access_token)
 
-#     access_token = auth_check()
+    #access_token = auth_check()
     if access_token != '':
         return 'it worked!'
     return 'failed'
-
-
-
-# app.run()
