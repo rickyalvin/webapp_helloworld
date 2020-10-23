1e1b0bf4f4951c6657781c4a6164b7b4951aeaab
rickyalvin <rickyalvin11@gmail.com>
2020-10-23 12:59:20 +1100

    Update app.py

--- app.py
+++ app.py
@@ -8,22 +8,22 @@ def home():
     return "Hello, World!"
 
 
-def auth_check():
-    url = 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F'
-    headers = {'Metadata' : true}
-    r = requests.get(url, headers=headers)
-    response = json.loads(r.content)
-    return response['access_token']
+# def auth_check():
+#     url = 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F'
+#     headers = {'Metadata' : true}
+#     r = requests.get(url, headers=headers)
+#     response = json.loads(r.content)
+#     return response['access_token']
     
     
 
-@app.route('/auth', methods = ['POST'])
-def get_sas_token():
+# @app.route('/auth', methods = ['POST'])
+# def get_sas_token():
     
-    access_token = auth_check()
-    if access_token != '':
-        return 'it worked!'
-    return 'failed'
+#     access_token = auth_check()
+#     if access_token != '':
+#         return 'it worked!'
+#     return 'failed'
 
 
 if __name__ == '__main__':
