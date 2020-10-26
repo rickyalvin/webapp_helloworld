3a762810e1dd755504f07ff96b7cb016a3e1fd38
chuang zhang <rickyalvin11@gmail.com>
2020-10-26 14:15:00 +1100

    add config file

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
 app = Flask(__name__)
 import requests
-# import json
+import json
+import config
+from azure.storage.blob import BlobServiceClient, generate_container_sas, ResourceTypes, AccountSasPermissions, generate_account_sas
+from datetime import datetime, timedelta
+
 
 @app.route("/",  methods = ['GET'])
 def home():
@@ -13,26 +17,46 @@ def json_parse(json_object):
     
     return client_id, client_secret
 
+def generate_token():
+    blob_service_client = BlobServiceClient(account_url=config.URL, credential=config.SHARED_KEY)
+    container_client = blob_service_client.get_container_client("mycontainer")
+
+    # container_token = generate_container_sas(
+    #             container_client.account_name,
+    #             container_client.container_name,
+    #             account_key=container_client.credential.account_key,
+    #             policy_id='my-access-policy-id'
+    #         )
+
+    sas_token = generate_account_sas(
+            blob_service_client.account_name,
+            account_key=blob_service_client.credential.account_key,
+            resource_types=ResourceTypes(object=True),
+            permission=AccountSasPermissions(read=True , write = True, add = True, create = True),
+            expiry=datetime.utcnow() + timedelta(hours=1)
+        )
+    return sas_token
+
 @app.route('/auth', methods = ['POST'])
 def get_sas_token():
     import adal
-
-    authentication_endpoint = 'https://login.microsoftonline.com/'
-    resource  = 'https://management.core.windows.net/'
-    storage_account = 'storagetestcz'
-    tenant_id = '24041256-c834-468a-b7e7-c4dc34322cfe'
     
-    resource = 'https://'+storage_account+'.blob.core.windows.net'
+    resource = 'https://'+config.STORAGE_ACCOUNT+'.blob.core.windows.net'
     
     application_id ,application_secret = json_parse(request.get_json())
 
     # get an Azure access token using the adal library
-    context = adal.AuthenticationContext(authentication_endpoint + tenant_id)
+    context = adal.AuthenticationContext(config.AUTHENTICATION_ENDPOINT + config.TENANT_ID)
     token_response = context.acquire_token_with_client_credentials(resource, application_id, application_secret)
 
     access_token = token_response.get('accessToken')
 
-    #access_token = auth_check()
+    #access_token = auth_check() 
+    ## if yes: generate the sas token
     if access_token != '':
-        return 'it worked!'
-    return 'failed'
+        sas_token = generate_token()
+        return sas_token
+    return 'wrong id or password'
+
+# if __name__ == '__main__':
+#     app.run()
---
+++ config.py
@@ -0,0 +1,13 @@
+
+## connection_string to connect the storage account: storagetestcz
+CONNECTION_STRING = 'BlobEndpoint=https://storagetestcz.blob.core.windows.net/;QueueEndpoint=https://storagetestcz.queue.core.windows.net/;FileEndpoint=https://storagetestcz.file.core.windows.net/;TableEndpoint=https://storagetestcz.table.core.windows.net/;SharedAccessSignature=sv=2019-12-12&ss=bfqt&srt=sco&sp=rwdlacupx&se=2020-10-26T10:19:50Z&st=2020-10-26T02:19:50Z&spr=https&sig=q%2F3NcHCG6yG1dyOLdGXFTJSzjOXB%2Fp%2BZcBkPgul%2BHwE%3D'
+# access key 
+SHARED_KEY = 'MYSEnNGsnMPHq7T+LMidGVtn7C0ryobMkLqPszTlMm+XX5SWSVuOLdn+47nHqrQtdnDMf8iB4XYdws+aV9Q94w=='
+
+URL = "https://storagetestcz.blob.core.windows.net"
+
+TENANT_ID = '24041256-c834-468a-b7e7-c4dc34322cfe'
+
+STORAGE_ACCOUNT = 'storagetestcz'
+
+AUTHENTICATION_ENDPOINT = 'https://login.microsoftonline.com/'
